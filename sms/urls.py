cba5739e42ff4c883376171d6cb94dbd7794f1ea
yewwoei <yewwoei.low@outlook.com>
2019-03-02 15:22:07 -0800

    finish react form, need to connect react to django

--- sms/urls.py
+++ sms/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.sms_response, name='sms')
+    path('api/register_user', views.register_user, name='register_user'),
+    path('', views.sms_response, name='sms'),
 ]
