fe5ca4811e7888a9574f28de9c063abdb0a87fd5
Fiona <fionatagious@gmail.com>
2019-03-02 03:26:01 -0800

    update models.py with class attributes

--- smsServer/models.py
+++ smsServer/models.py
@@ -1,3 +1,16 @@
+
+
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    registration_date = models.DateTimeField('patient registered')
+
+
+class Medication(models.Model):
+    med_name = models.CharField(max_length = 10)
+    dosage = models.IntegerField(default=0)
+    unit = models.CharField(max_length = 5)
+    frequency = models.IntegerField(default=0)
