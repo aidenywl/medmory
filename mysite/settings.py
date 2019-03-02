a722eb9b49ebaa2d0b460fb8e186887ad4ff55ed
Fiona <fionatagious@gmail.com>
2019-03-02 13:10:55 -0800

    add 'sms.apps.SmsConfig' to INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.ngrok.io']
 # Application definition
 
 INSTALLED_APPS = [
+    'sms.apps.SmsConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
