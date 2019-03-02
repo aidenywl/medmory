3c86d039ce0f99e0bd5a9c299b63d67f56a99e5e
Fiona <fionatagious@gmail.com>
2019-03-02 03:10:42 -0800

    add html

--- smsServer/frontend/src/components/App.js
+++ smsServer/frontend/src/components/App.js
@@ -2,7 +2,31 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <div>"Fiona and David, you can fill in code here."</div>;
+    return (<div> 
+
+
+ <form>
+  First name:
+  <input type="text" name="firstname"/>< br />
+  Last name:
+  <input type="text" name="lastname"/>< br />
+  Phone number:
+  <input type="text" name="lastname"/>< br />
+  <input type="radio" name="gender" value="male" checked/> Male
+  <input type="radio" name="gender" value="female"/> Female
+  <input type="radio" name="gender" value="other"/> Other< br />
+  Medication Name:
+  <input type="text" name="medication"/>< br />
+  Dosage:
+  <input type="text" name="dosage"/>< br />
+  Frequency:
+  <input type="text" name="dosage"/>< br />
+  Time Period:
+  <input type="text" name="dosage"/>< br />  
+    
+</form>  
+
+  </div>);
   }
 }
 
