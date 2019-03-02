ab2aeb902c9cf6ad33cc432ce8eeaeb155ba050e
yewwoei <yewwoei.low@outlook.com>
2019-03-02 11:54:22 -0800

    push view method for form

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Tijuana'
 
 USE_I18N = True
 
--- sms/views.py
+++ sms/views.py
@@ -1,13 +1,27 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.http import HttpResponseServerError, HttpResponse
 from twilio.twiml.messaging_response import MessagingResponse
 # Create your views here.
+
+import json
+
+
+def register_user(request):
+    if request.method != 'POST':
+        return HttpResponseServerError('wrong method used.')
+    request_data = json.loads(request.body)
+
+    # save the data into the database
+
+    # communicate with twillo
+
+    # return the code to be used.
 
 
 @csrf_exempt
 def sms_response(request):
-    # start our TwiML response
+        # start our TwiML response
     resp = MessagingResponse()
 
     # add a text message
